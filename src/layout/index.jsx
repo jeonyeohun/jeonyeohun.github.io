23d33625309982f4a9f42a55923513161f6e9743
Jeon, Yeo Hun <21500630@handong.edu>
2021-06-29 23:43:56 +0900

    :sparkles: feat: add scroll-to-top button

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -5,11 +5,22 @@ import { Header } from '../components/header'
 import { ThemeSwitch } from '../components/theme-switch'
 import { Footer } from '../components/footer'
 import { rhythm } from '../utils/typography'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 
 import './index.scss'
 
 export const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    })
+  }
+
+  
 
   return (
     <React.Fragment>
@@ -25,6 +36,25 @@ export const Layout = ({ location, title, children }) => {
         <ThemeSwitch />
         <Header title={title} location={location} rootPath={rootPath} />
         {children}
+        
+        <div onClick={scrollToTop}
+        style={{
+          position: 'fixed',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          bottom: 30,
+          right: 30,
+          cursor: 'pointer',
+          padding: 5,
+          backgroundColor: '#e9ecef',
+          opacity: '70%',
+          borderRadius: 10,
+          color: '#22179d'
+        }}>
+          <ArrowUpwardIcon fontSize="large" />
+        </div>
+        
         <Footer />
       </div>
     </React.Fragment>
